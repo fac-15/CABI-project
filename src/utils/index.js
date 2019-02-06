8a47e99508870a9e270d283be8af687e1ec8dda8
mike <boimik87@gmail.com>
2019-02-06 14:39:07 +0000

    server moved into src folder

---
+++ src/utils/index.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import '../../public/index.css';
+import App from '../components/App';
+// import { render } from 'react-dom';
+
+// const App = () => <h1>Hello World</h1>;
+
+ReactDOM.render(<App />, document.getElementById('root'));
