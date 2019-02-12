cac28c66b8d404803362191b42d13ac322dba13f
Lucy <lewright20@gmail.com>
2019-02-12 10:47:08 +0000

    created components Risk Dashbord PestBox PestButton RiskLevel

--- src/components/Dashboard.js
+++ src/components/Dashboard.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import jsonGhana from '../../factsheets_Ghana.json';
+import PestBox from './PestBox';
 
-const Dashboard = () => (
-            <div>
-                    <h1 className="App-Title">Hello Dashboard! Muahahahahaha</h1>
-            </div>
+const Dashboard = props => (
+    <div>
+        <h1 className="App-Title">Hello Dashboard! Muahahahahaha</h1>
+        <PestBox cropName={props.crop} pests={props.data} />
+    </div>
 );
 
 export default Dashboard;
--- src/components/Risk.js
+++ src/components/Risk.js
@@ -1,3 +1,17 @@
 import React from 'react';
 import jsonGhana from '../../factsheets_Ghana.json';
-import { BrowserRouter as Router, Link } from "react-router-dom";
+import { BrowserRouter as Router, Link } from 'react-router-dom';
+import PestButton from './PestButton';
+
+const Risk = props => (
+    <div>
+        <h1>Hello Risk!</h1>
+        <ul>
+            {props.data.map(pest => (
+                <PestButton pest={pest} />
+            ))}
+        </ul>
+    </div>
+);
+
+export default Risk;
