1f7a6683952bb60f7ee86c13c100d45fe74808ab
saksiva <sakatheesan@gmail.com>
2019-02-13 12:58:23 +0000

    location does work

---
+++ src/components/Location.js
@@ -0,0 +1,10 @@
+import React, { Component } from "react";
+import { BrowserRouter as Router, Link } from "react-router-dom";
+import { CountryDropdown, RegionDropdown } from "react-country-region-selector";
+
+class Location extends Component {
+  constructor(props) {
+    super(props);
+    
+
+export default Location;
--- src/components/Search.js
+++ src/components/Search.js
@@ -1,16 +1,19 @@
 import React from "react";
 import jsonGhana from "../../factsheets_Ghana.json";
 import { BrowserRouter as Router, Link } from "react-router-dom";
+import Location from "./Location";
 
 const Search = props => (
   <div>
     <h1 className="App-Title">Hello World</h1>
     <Link to="/home">Home</Link>
-    {jsonGhana.Factsheets.map(i => {
-      if (i.PAN) return <p> {i.PAN} </p>;
-    })}
-    <p className="App-intro">{props.data}</p>
+    <Location />
   </div>
 );
 
 export default Search;
+
+// {jsonGhana.Factsheets.map(i => {
+//   if (i.PAN) return <p> {i.PAN} </p>;
+// })}
+// <p className="App-intro">{props.data}</p>
