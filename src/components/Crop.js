8e210a4a222a982489f37556afd470ed5571ef82
Lucy <lewright20@gmail.com>
2019-02-15 14:23:02 +0000

    [experiment] trying to fetch json object

--- src/components/Crop.js
+++ src/components/Crop.js
@@ -1,19 +1,53 @@
-import React from "react";
-import { BrowserRouter as Router, Link } from "react-router-dom";
-import HowManyFarmers from "./HowManyFarmers.js";
-import Header from "./Header";
-import Title from "./Title.js";
+import React from 'react';
+// import { BrowserRouter as Router, Link } from 'react-router-dom';
+import HowManyFarmers from './HowManyFarmers.js';
+import Header from './Header';
+import Title from './Title.js';
+// import fullJson from '../data/factsheet.json';
 
-export default function Crop(props) {
+class Crop extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            info: null
+        };
+    }
 
-  return (
-    <div>
-      <Title />
-      {console.log(props.match.params.crops)}
-      <Header name={props.match.params.crops} />
-      <div id="container">
-            <HowManyFarmers />
+    componentDidMount = async () => {
+        // await fetch('../data/factsheet.json')
+        //     .then(res => res.json())
+        //     .then(data => {
+        //         console.log(data.CommonName);
+        //         this.setState({
+        //             info: data[0].CommonName
+        //         });
+        //     });
+        const myHeaders = new Headers({
+            'Content-Type': 'application/json',
+            Accept: 'application/json'
+        });
+        await fetch('../data/factsheet.json', { headers: myHeaders })
+            .then(res => res.json())
+            .then(data => {
+                this.setState({
+                    info: data
+                });
+            });
+    };
+
+    render() {
+        return (
+            <div>
+                <Title />
+                {console.log('data: ', JSON.stringify(this.state.info))}
+                {console.log(this.props.match.params.crops)}
+                <Header name={this.props.match.params.crops} />
+                <div id="container">
+                    <HowManyFarmers />
+                </div>
             </div>
-    </div>
-  );
+        );
+    }
 }
+
+export default Crop;
