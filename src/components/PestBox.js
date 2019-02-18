b66934a251e752fcef3a4160c17ed9cb6a948264
Sandra <grahlsan@gmail.com>
2019-02-18 16:47:06 +0000

    more styling changes

--- src/components/PestBox.js
+++ src/components/PestBox.js
@@ -2,11 +2,13 @@ import React from 'react';
 import PestButton from './PestButton';
 
 const PestBox = props => (
-    <div className="pestBox">
+    <div className="wrapper">
+        <div className="pestBox">
         {console.log(props)}
         {props.pests.map(pest => (
             <PestButton pest={pest} route="/pest" />
         ))}
+        </div>
     </div>
 );
 
