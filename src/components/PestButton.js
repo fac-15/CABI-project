703dd7a90a9f5d328f44b2e77ee57b15d7e06a9d
mike <boimik87@gmail.com>
2019-02-20 16:44:23 +0000

    added key in maps of PestButton.js

--- src/components/PestButton.js
+++ src/components/PestButton.js
@@ -1,29 +1,34 @@
 import React from 'react';
 import RiskLevel from './RiskLevel';
-import { BrowserRouter as Router, Link } from "react-router-dom";
-import factsheet from '../data/factsheet'
+import { BrowserRouter as Router, Link } from 'react-router-dom';
+import factsheet from '../data/factsheet';
 
 export default function PestButton({ pest, country, route }) {
-    const { name, risk} = pest;
+    const { name, risk } = pest;
 
     return (
         <div className="pestBtn">
             <Link to={route}>
-                {factsheet.map((e) => {
-                        let commonName = "";
-                    if (name === e.PestScientificName && 
-                        country === (e.CountryISO === 'GH'
-                    ? 'Ghana'
-                    : e.CountryISO === 'KE'
-                    ? 'Kenya'
-                    : e.CountryISO === 'ZM'
-                    ? 'Zambia'
-                    : '')) {
-                        commonName = e.CommonName
-                        return <button>
-                        <h3>{commonName}</h3>
-                        <RiskLevel level={risk} />
-                        </button>
+                {factsheet.map((e, key) => {
+                    let commonName = '';
+                    if (
+                        name === e.PestScientificName &&
+                        country ===
+                            (e.CountryISO === 'GH'
+                                ? 'Ghana'
+                                : e.CountryISO === 'KE'
+                                ? 'Kenya'
+                                : e.CountryISO === 'ZM'
+                                ? 'Zambia'
+                                : '')
+                    ) {
+                        commonName = e.CommonName;
+                        return (
+                            <button key={key}>
+                                <h3>{commonName}</h3>
+                                <RiskLevel level={risk} />
+                            </button>
+                        );
                     }
                 })}
             </Link>
