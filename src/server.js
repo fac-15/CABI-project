d7bd78fd73830b33c18733e8938dc8ea65c2a6f5
saksiva <sakatheesan@gmail.com>
2019-02-20 14:26:54 +0000

    deploy

--- src/server.js
+++ src/server.js
@@ -1,32 +1,33 @@
-const express = require('express');
+const express = require("express");
 const app = express();
 // const fs = require("fs");
 const port = process.env.PORT || 5000;
-const Api = require('./api');
-const cors = require('cors');
+const Api = require("./api");
+const cors = require("cors");
+const path = require("path");
 
 app.use(cors());
 
-
 app.get("/riskres", (req, res) => {
-  Api().then(result => {
-    res.send(result);})
-    .catch(err => console.log(err))
+  Api()
+    .then(result => {
+      res.send(result);
+    })
+    .catch(err => console.log(err));
 });
 
 //console.log that your server is up and running
 app.listen(port, () => console.log(`Listening on port ${port}`));
 
-
 //create a GET route
-app.get('/express_backend', (req, res) => {
-    res.send({ express: 'YOUR EXPRESS BACKEND IS CONNECTED TO REACT' });
+app.get("/express_backend", (req, res) => {
+  res.send({ express: "YOUR EXPRESS BACKEND IS CONNECTED TO REACT" });
 });
 
-// app.get('*', (req, res) => {
-//     res.sendFile(path.resolve('public', 'index.html'));
-// });
+app.get("*", (req, res) => {
+  res.sendFile(path.resolve("public", "index.html"));
+});
 
-app.get('/crop', (req, res, next) => {
-    res.send(factsheet);
+app.get("/crop", (req, res, next) => {
+  res.send(factsheet);
 });
