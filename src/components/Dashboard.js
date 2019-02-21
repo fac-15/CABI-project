72c371dd39d1acd16ccdd67e584b86168bd278c9
saksiva <sakatheesan@gmail.com>
2019-02-21 16:29:02 +0000

    deploy

--- src/components/Dashboard.js
+++ src/components/Dashboard.js
@@ -1,88 +1,85 @@
-import React from 'react';
-import PestBox from './PestBox';
-import Button from './Button.js';
-import CropBox from './CropBox';
-import Title from './Title';
-import factSheet from '../data/factsheet';
-import { DashWrap } from './styledComponents';
+import React from "react";
+import PestBox from "./PestBox";
+import Button from "./Button.js";
+import CropBox from "./CropBox";
+import Title from "./Title";
+import factSheet from "../data/factsheet";
+import { DashWrap } from "./styledComponents";
 
 const countryISOs = {
-    Kenya: 'KE',
-    Ghana: 'GH',
-    Zambia: 'ZM'
+  Kenya: "KE",
+  Ghana: "GH",
+  Zambia: "ZM"
 };
 
 class Dashboard extends React.Component {
-    constructor(props) {
-        super(props);
+  constructor(props) {
+    super(props);
 
-
-        this.state = {
-            country: '',
-            region: '',
-            riskAndName: null,
-            crop: '',
-            loading: true
-        };
-    }
-    componentDidMount() {
-        const { country, region } = this.props.location.state;
-        this.setState({
-            country: country,
-            region: region
-
-        });
-        this.callBackendAPI(region)
-            .then(res => res.json())
-            .then(body => {
-                const reversedBody = body.results.reverse();
-                const oneName = [];
-                const map = new Map();
-                for (const item of reversedBody) {
-                    if (!map.has(item.pest_name)) {
-                        map.set(item.pest_name, true);
-                        oneName.push({ name: item.pest_name, risk: item.risk });
-                    }
-                }
-                const riskAndName = oneName.map(obj => {
-                    return {
-                        name: obj.name,
-                        risk: obj.risk
-                    };
-                });
-
-                this.setState({ riskAndName });
-            })
-            .catch(err => console.log('error in routes req', err));
-    }
-
-    callBackendAPI = async region => {
-        const response = await fetch(
-            `http://localhost:5000/riskres?region=${region}`
-        );
-        return response;
+    this.state = {
+      country: "",
+      region: "",
+      riskAndName: null,
+      crop: "",
+      loading: true
     };
-
-    separatePestByCrop = crop => {
-        const countryISO = countryISOs[this.state.country];
-
-        if (this.state.riskAndName == null) {
-            return [];
+  }
+  componentDidMount() {
+    const { country, region } = this.props.location.state;
+    this.setState({
+      country: country,
+      region: region
+    });
+    this.callBackendAPI(region)
+      .then(res => res.json())
+      .then(body => {
+        const reversedBody = body.results.reverse();
+        const oneName = [];
+        const map = new Map();
+        for (const item of reversedBody) {
+          if (!map.has(item.pest_name)) {
+            map.set(item.pest_name, true);
+            oneName.push({ name: item.pest_name, risk: item.risk });
+          }
         }
-        return this.state.riskAndName
-            .map(pest => {
-                return {
-                    ...pest,
-                    ...factSheet.find(
-                        item =>
-                            item.PestScientificName === pest.name &&
-                            item.CountryISO === countryISO
-                    )
-                };
-            })
-            .filter(pest => pest.Crop === crop);
-    };
+        const riskAndName = oneName.map(obj => {
+          return {
+            name: obj.name,
+            risk: obj.risk
+          };
+        });
 
+        this.setState({ riskAndName });
+      })
+      .catch(err => console.log("error in routes req", err));
+  }
+
+  callBackendAPI = async region => {
+    const response = await fetch(
+      `http://localhost:5000/riskres?region=${region}`
+    );
+    return response;
+  };
+
+  separatePestByCrop = crop => {
+    const countryISO = countryISOs[this.state.country];
+
+    if (this.state.riskAndName == null) {
+      return [];
+    }
+    return this.state.riskAndName
+      .map(pest => {
+        return {
+          ...pest,
+          ...factSheet.find(
+            item =>
+              item.PestScientificName === pest.name &&
+              item.CountryISO === countryISO
+          )
+        };
+      })
+      .filter(pest => pest.Crop === crop);
+  };
 
   render() {
     if (this.state.riskAndName === null) {
@@ -120,6 +117,7 @@ class Dashboard extends React.Component {
         </div>
       );
     }
+  }
 }
 
 export default Dashboard;
